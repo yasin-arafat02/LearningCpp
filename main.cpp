9734f3c3292e92d111b99982a641eb57b8e7caff
yasin-arafat02 <arn.shibloo02@gmail.com>
2025-07-22 12:12:09 +0600

    edit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <climits>
 using namespace std;
-bool result(int a){
-   if(a>39){
-    return true;
-   }
-   else return false;
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    cout<<boolalpha;
-    cout<<result(5)<<endl;
-    cout<<result(60)<<endl;
-    cout<<result(39)<<endl;
 
+    int n;cin>>n;
+    vector<int> v;
+    for (int i = 0; i < n; i++){
+        int x;cin>>x;
+        v.push_back(x);
+    }
+    int frq ;cin>>frq;
+    int count=0;
+    for (int i = n-1; i >= 0; i--)
+    {
+        if(frq==v[i])count++;
+    }
+    cout<<count;
 }
